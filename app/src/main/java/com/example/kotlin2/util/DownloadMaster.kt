382dbe0cf78b1ad037cdfbb5619045910ceae3a7
Azimus <millennium1313>
2020-01-08 13:40:02 +0600

    show downloading is completed notification

--- app/src/main/java/com/example/kotlin2/util/DownloadMaster.kt
+++ app/src/main/java/com/example/kotlin2/util/DownloadMaster.kt
@@ -4,6 +4,8 @@ import android.app.DownloadManager
 import android.content.Context
 import android.content.Context.DOWNLOAD_SERVICE
 import android.net.Uri
+import android.opengl.Visibility
+import android.os.Build
 import java.io.File
 
 class DownloadMaster  {
@@ -12,14 +14,19 @@ class DownloadMaster  {
     fun downloadFile(context: Context, url: String, fileName: String?) {
         file.mkdir()
         var request: DownloadManager.Request =
-            DownloadManager.Request(Uri.parse(url))
-                .setTitle("$fileName") // Title of the Download Notification
-                .setDescription("Downloading") // Description of the Download Notification
-                .setDestinationUri(Uri.fromFile(file))
-                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE) // Visibility of the download Notification
-                .setAllowedOverMetered(true) // Set if download is allowed on Mobile network
-                .setAllowedOverRoaming(true)
-                .setDestinationInExternalPublicDir(file.toString(), fileName)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                DownloadManager.Request(Uri.parse(url))
+                    .setTitle("$fileName") // Title of the Download Notification
+                    .setDescription("Downloading...") // Description of the Download Notification
+                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE) // Visibility of the download Notification
+                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                    .setAllowedOverMetered(true) // Set if download is allowed on Mobile network
+                    .setAllowedOverRoaming(true)
+                    .setRequiresCharging(false)
+                    .setDestinationInExternalPublicDir(file.toString(), fileName)
+            } else {
+                TODO("VERSION.SDK_INT < N")
+            }
 
         val downloadManager: DownloadManager = context.getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadManager.enqueue(request)
